48e1b0d718ac893fb66fb73aefa909178cb8a8ea
Heidi Dong <dong.heidi@gmail.com>
2016-09-19 01:55:14 -0700

    add Twitter library and example

---
+++ src/proclub/twitter/TwitterAPI.java
@@ -0,0 +1,24 @@
+package proclub.twitter;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+public class TwitterAPI {
+	
+	public TwitterAPI() {		
+	}
+	
+	public static Twitter getApi() {
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+		cb.setDebugEnabled(true)
+		  .setOAuthConsumerKey("HvceKDr60zc7YvFMBIZTcuJy7")
+		  .setOAuthConsumerSecret("7Y4BFwDMjurg1oWakFfHyy65m6xbcEtJ3PqbSr3YmonCXPjJBV")
+		  .setOAuthAccessToken("776668206257745921-5JPypDTrfdeDcGZ8UOzERHWBIyz3JMU")
+		  .setOAuthAccessTokenSecret("vp25u81HfkoqfrEwIPB7yGnQ2qtldSV503DchthwQ6Uun");
+		TwitterFactory tf = new TwitterFactory(cb.build());
+		Twitter twitter = tf.getInstance();
+		return twitter;
+	}
+
+}
