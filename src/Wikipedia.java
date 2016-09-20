3186abd89b1b2044349b3339a372fa551f1edb89
varun-tandon <vrntandon@gmail.com>
2016-09-19 17:31:58 -0700

    Added basic code for members to start with

--- src/Wikipedia.java
+++ src/Wikipedia.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
+import javax.security.auth.login.LoginException;
+
+import jwiki.core.Wiki;
 
 public class Wikipedia {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws LoginException {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Username:");
+		String username = s.nextLine();
+		System.out.println("Password:");
+		String password = s.nextLine();
+		Wiki wiki = new Wiki(username, password, "en.wikipedia.org");
+		
 
 	}
 
